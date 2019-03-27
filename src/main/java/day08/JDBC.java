a6f6a286dca72849da73e380ce5c09a3a2cbc463
zhaochenjin <984185626@qq.com>
2019-03-27 15:12:46 +0800

    updae

--- src/main/java/day08/JDBC.java
+++ src/main/java/day08/JDBC.java
@@ -1,6 +1,6 @@
 package day08;
 
-// import sun.security.util.Password;
+import com.mysql.jdbc.Driver;
 
 import java.sql.*;
 
@@ -10,7 +10,7 @@ public class JDBC {
     private static  final String Password="123456";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //1.Driver加载驱动
+        //1.Driver加载驱动类，装载MySQL驱动
         // new Driver();
         Class.forName("com.mysql.jdbc.Driver");
         //2.Connection //获取连接
@@ -18,10 +18,15 @@ public class JDBC {
         //3.PrepareStatement 创建接口实例
         String sql="insert into db_test.user value(null,?,md5(?))";
         PreparedStatement preparedStatement=connection.prepareStatement(sql);
-        preparedStatement.setString(1,"Tom");
+        preparedStatement.setString(1,"Tom");//参数索引是从1开始计算，而不是0
         preparedStatement.setString(2,"123");
+
+        //preparedStatement.setObject(1,"zhao"); // setObject 所有类型
+        //preparedStatement.setObject(2,"123");
+        //问题1 ：同时注入两个为什么只出现一个。下面的运行问题
+        //preparedStatement.setDate(3,new java.sql.Date(System.currentTimeMillis())); // 注入当前时间
         //4.1.p.execteUpdate(); // DML
-        preparedStatement.executeUpdate(); // 更新
+        preparedStatement.executeUpdate(); // 更新.执行
         //4.2.p.executeQuery();// DQL
         //5. *.close();
         preparedStatement.close(); //
--- src/main/java/day08/MysqlCommandLine.java
+++ src/main/java/day08/MysqlCommandLine.java
@@ -1,6 +1,12 @@
 package day08;
 
-import java.util.Scanner;
+import com.mysql.jdbc.Driver;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
 
 /*
     create database ...;
@@ -13,14 +19,90 @@ import java.util.Scanner;
     drop database ...;
  */
 public class MysqlCommandLine {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306?user=root&password=123456&useSSL=false";
+
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+    private static Scanner scanner;
+    private static SimpleDateFormat simpleDateFormat;
+
+    public MysqlCommandLine() {
+        scanner = new Scanner(System.in);
+        getConnection();
+        simpleDateFormat = new SimpleDateFormat("[YYYY-MM-dd HH:mm:ss]");
+    }
+
+    public Connection getConnection() { // singleton 单例模式
+        if (connection == null) {
+            try {
+                new Driver(); // 加载驱动器
+                connection = DriverManager.getConnection(URL);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+
+    public void update(String sql) {
+        try {
+            long start = System.currentTimeMillis(); // 开始时间
+            preparedStatement = connection.prepareStatement(sql);
+            int rowAffected = preparedStatement.executeUpdate();
+            long end = System.currentTimeMillis(); // 结束时间
+
+            System.out.print(simpleDateFormat.format(new Date(end)));
+            System.out.println(rowAffected + " row affected in " + (end - start) + " ms"); // 用时
+        } catch (SQLException e) {
+            System.out.println("ERROR" + e.getMessage());
+        }
+    }
+
+
+    public void query(String sql) {
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData(); // 元数据
+            System.out.println(resultSetMetaData.getColumnCount());            // 3
+            System.out.println(resultSetMetaData.getColumnClassName(1));       // java.lang.Integer
+            System.out.println(resultSetMetaData.getColumnDisplaySize(1));     // 11
+            System.out.println(resultSetMetaData.getColumnLabel(1));           // id
+            System.out.println(resultSetMetaData.getColumnName(1));            // id
+            System.out.println(resultSetMetaData.getColumnType(1));            // 4
+            System.out.println(resultSetMetaData.getColumnTypeName(1));        // INT
+            while (resultSet.next()) {
+                // TODO: 2019/3/27
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void dispatch(String sql) {
+        if (sql.toLowerCase().trim().startsWith("select")) { // toLowerCase变小写 trim消除空格
+            query(sql);
+        } else {
+            update(sql);
+        }
+    }
+
+    public String getSQL() {
+        System.out.println("mysql> ");
+        String sql = scanner.nextLine();
+        return sql;
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("input SQL statement: ");
-        String sql = scanner.nextLine();
-        // TODO: 2019/3/26
-        // .executeUpdate();
-        // .executeQuery();   // select
-        System.out.println(sql);
+        MysqlCommandLine mysqlCommandLine = new MysqlCommandLine();
+        String sql = mysqlCommandLine.getSQL();
+        while (!sql.equalsIgnoreCase("quit")) {
+            mysqlCommandLine.dispatch(sql);
+            sql = mysqlCommandLine.getSQL();
+        }
     }
 }
